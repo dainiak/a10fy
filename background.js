b84fc262098e544d84ef1192c40f0f90cf951f02
dainiak <dainiak@gmail.com>
2024-06-04 10:51:14 +0400

    add html skeletonization

--- background.js
+++ background.js
@@ -1,22 +1,16 @@
-// chrome.action.onClicked.addListener((tab) => {
-//     chrome.scripting.executeScript({
-//         target: {tabId: tab.id},
-//         files: ["content-script.js"]
-//     });
-// });
-
-
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
-        console.log(sender.tab ?
-            "from a content script:" + sender.tab.url :
-            "from the extension");
-        if (request.greeting === "hello"){
-            chrome.tts.speak("Hello, world.", {"lang": "en-US", "rate": 1.0});
-            sendResponse({farewell: "goodbye"});
-        }
+        console.log(sender.tab ? "from a content script:" + sender.tab.url : "from the extension");
+        console.log(request);
 
-        chrome.tabs.captureVisibleTab(
+        // if (request.greeting === "hello"){
+        //     chrome.tts.speak("Hello, world.", {"lang": "en-US", "rate": 1.0});
+        //     sendResponse({
+        //         farewell: "goodbye"
+        //     });
+        // }
+
+        /*chrome.tabs.captureVisibleTab(
             {
                 "format": "jpeg",
                 "quality": 40
@@ -27,7 +21,9 @@ chrome.runtime.onMessage.addListener(
                 "contents": [
                     {
                         "parts":[
-                            {"text": "What is this picture?"},
+                            {
+                                "text": "Here is a screenshot of a website. Describe this website for a visually impaired person."
+                            },
                             {
                                 "inline_data": {
                                     "mime_type": "image/jpeg",
@@ -38,23 +34,21 @@ chrome.runtime.onMessage.addListener(
                     }
                 ]
             };
-            let geminiRequestHeaders = {
-                "Content-Type": "application/json"
-            };
             let GOOGLE_API_KEY = "AIzaSyD4YqBxteEa_aAR4wr1VEMNsMJJdnCkVXQ";
             let geminiUrl = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${GOOGLE_API_KEY}`;
             fetch(geminiUrl, {
                 "method": "POST",
-                "headers": geminiRequestHeaders,
+                "headers": {"Content-Type": "application/json"},
                 "body": JSON.stringify(geminiRequestData)
             }).then((response) => {
                 return response.json();
             }).then((data) => {
-                console.log(data);
-                // let geminiResponse = data["contents"][0]["parts"][1]["text"];
+                // console.log(data);
+                let geminiResponse = data.candidates[0].content.parts[0]["text"];
                 // chrome.tts.speak(geminiResponse, {"lang": "en-US", "rate": 1.0});
+                console.log(geminiResponse);
             });
-        })
+        })*/
     }
 );
 
--- content-script.js
+++ content-script.js
@@ -1,3 +1,123 @@
-console.log("content-script.js");
+(() => {
+    const cssPrefix = "a10fy_";
+    function injectCssClasses() {
+        Array.from(document.getElementsByTagName("*")).forEach(
+            (element, idx) => {
+                element.classList.add(cssPrefix + idx);
+            }
+        );
+    }
 
-chrome.runtime.sendMessage({greeting: "hello"});
+    function performAction(action, index, value) {
+        const element = document.querySelector(`.${cssPrefix}${index}`);
+        if (!element)
+            return;
+        switch (action) {
+            case "click":
+                element.click();
+                break;
+            case "focus":
+                element.focus();
+                break;
+            case "scrollIntoView":
+                element.scrollIntoView();
+                break;
+            case "select":
+                element.select();
+                break;
+            case "submit":
+                element.submit();
+                break;
+            case "setChecked":
+                element.checked = true;
+                break;
+            case "setUnchecked":
+                element.checked = false;
+                break;
+            case "setValue":
+                element.value = value;
+                break;
+            case "remove":
+                element.remove();
+                break;
+            case "hide":
+                element.style.display = "none";
+                break;
+        }
+    }
+
+    chrome.runtime.onMessage.addListener(
+        function(request, sender, sendResponse) {
+            if (request.action) {
+                performAction(action.action, action.index, action.value);
+            }
+            else if (request.actions) {
+                request.actions.forEach(
+                    (action) => {
+                        performAction(action.action, action.index, action.value);
+                    }
+                );
+            }
+        }
+    );
+
+    function getHtmlSkeleton(htmlString) {
+        const tempDiv = document.createElement("div");
+        tempDiv.innerHTML = htmlString;
+        Array.from(tempDiv.getElementsByTagName("*")).forEach(
+            (node) => {
+                // if node is a text element, remove all leading and trailing white spaces
+                if(node.nodeType === Node.TEXT_NODE){
+                    if(!node.previousSibling)
+                        node.textContent = node.textContent.replace(/^(&nbsp;|\s)*/, "")
+                    if(!node.nextSibling)
+                        node.textContent = node.textContent.replace(/(&nbsp;|\s)*$/, "")
+
+                    node.textContent = node.textContent.replace(/(&nbsp;|\s)+/g, " ")
+                    console.log(node.textContent);
+                }
+                else if (node.nodeType === Node.COMMENT_NODE){
+                    node.remove();
+                }
+                else if (node.tagName.toLowerCase() === "script" && node.innerHTML && node.innerHTML.trim()){
+                    node.innerHTML = "/* some script here */";
+                }
+                else if (node.tagName.toLowerCase() === "style" && node.innerHTML && node.innerHTML.trim()){
+                    node.innerHTML = "/* some style here */";
+                }
+                else if (node.tagName.toLowerCase() === "noscript"){
+                    node.remove();
+                }
+
+                // remove all data attributes
+                Object.keys(node.dataset).forEach(key => {delete node.dataset[key]});
+                // remove inner content of svg elements
+                if(node.tagName === "svg") {
+                    node.innerHTML = "";
+                }
+                // remove all classes that do not start with cssPrefix
+                Array.from(node.classList).forEach(
+                    (className) => !className.startsWith(cssPrefix) && node.classList.remove(className)
+                );
+            }
+        );
+        let htmlSkeleton = tempDiv.innerHTML;
+        tempDiv.remove();
+        return htmlSkeleton.trim().replace(/>\s+</g, "><");
+    }
+
+    window.addEventListener("load", () => {
+        injectCssClasses();
+
+        chrome.runtime.sendMessage({
+            documentHTML: getHtmlSkeleton(document.body.innerHTML),
+            documentText: document.body.innerText,
+            documentURL: document.location.href,
+            documentTitle: document.title,
+        });
+    });
+
+    // window.navigation.addEventListener("navigate", () => {
+    //     console.log("page changed");
+    // });
+})();
