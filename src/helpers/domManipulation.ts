786ef85072a78357e774846550c6c5dfad1bad1f
Alex Dainiak <alex.dainiak@yandex.ru>
2024-06-16 20:22:08 +0400

    support :before/:after pseudo-elements during html simplification

--- src/helpers/domManipulation.ts
+++ src/helpers/domManipulation.ts
@@ -26,17 +26,19 @@ function findElementByIndex(index: number | string | null) {
 
 type DocumentSkeletonizationOptions = {
     wrapTextNodes?: boolean;
+    revealPseudoElements?: boolean;
 }
 
-function getDocumentSkeleton(rootElement: HTMLElement, options: DocumentSkeletonizationOptions = {}):string {
+function getDocumentSkeleton(rootElement: HTMLElement, options: DocumentSkeletonizationOptions = {}): string {
     const wrapTextNodes = options?.wrapTextNodes || false;
+    const revealPseudoElements = options?.revealPseudoElements || false;
 
     elementMap.clear();
     let nodeIndex = 0;
 
-    function getSimplifiedDomRecursive(node: Node, keepWhitespace: boolean)  {
+    function getSimplifiedDomRecursive(node: Node, keepWhitespace: boolean): Node[] {
         if (!(node instanceof Text || node instanceof HTMLElement || node instanceof Comment))
-            return null;
+            return [];
         keepWhitespace ||= false;
         if (node instanceof Text) {
             const text = (keepWhitespace ? node.textContent : node.textContent?.replace(/(&nbsp;|\s|\n)+/g, " ")) || "";
@@ -45,67 +47,86 @@ function getDocumentSkeleton(rootElement: HTMLElement, options: DocumentSkeleton
                 wrapper.textContent = text;
                 wrapper.setAttribute("class", `${cssPrefix}${nodeIndex}`);
                 ++nodeIndex;
-                // @ts-ignore
-                node[cssPrefixFallbackSymbol] = nodeIndex;
+                Object.assign(node, {[cssPrefixFallbackSymbol]: nodeIndex})
                 elementMap.set(nodeIndex, node);
-                return wrapper;
+                return [wrapper];
             }
-            return document.createTextNode(text);
+            return [document.createTextNode(text)];
         } else if (node instanceof Comment || ["script", "style", "noscript"].includes(node.tagName.toLowerCase())) {
-            return null;
+            return [];
         } else if (node instanceof HTMLElement) {
+            const result = [];
             const nodeStyle = window.getComputedStyle(node);
             const resultNode = node.cloneNode(false) as HTMLElement;
 
+            if (revealPseudoElements) {
+                const pseudoStyleBefore = window.getComputedStyle(node, "before");
+                let contentBefore = pseudoStyleBefore.getPropertyValue("content");
+                contentBefore = contentBefore === "none" ? "" : contentBefore.replace(/["']/g, "").trim();
+                if (pseudoStyleBefore.getPropertyValue("display") !== "none" && contentBefore)
+                    result.push(document.createTextNode(contentBefore));
+            }
+            result.push(resultNode);
+            if (revealPseudoElements) {
+                const pseudoStyleAfter = window.getComputedStyle(node, "after");
+                let contentAfter = pseudoStyleAfter.getPropertyValue("content");
+                contentAfter = contentAfter === "none" ? "" : contentAfter.replace(/["']/g, "").trim();
+                if (pseudoStyleAfter.getPropertyValue("display") !== "none" && contentAfter)
+                    result.push(document.createTextNode(contentAfter));
+            }
+
             ++nodeIndex;
             resultNode.setAttribute("class", `${cssPrefix}${nodeIndex}`);
-            // @ts-ignore
-            node[cssPrefixFallbackSymbol] = nodeIndex;
+            Object.assign(node, {[cssPrefixFallbackSymbol]: nodeIndex});
             elementMap.set(nodeIndex, node);
 
             const srcAttribute = node.getAttribute("src");
             if (srcAttribute?.startsWith("data:")) {
                 resultNode.removeAttribute("src");
             }
-            for (let i = 0; i < resultNode.attributes.length; i++) {
-                const attribute = resultNode.attributes[i];
-                if (attribute.name.startsWith("data-")) {
-                    resultNode.removeAttribute(attribute.name);
-                }
-            }
+
+            Array.from(resultNode.attributes).forEach(
+                attr => attr.name.startsWith("data-") && resultNode.removeAttribute(attr.name)
+            );
 
             if (["svg"].includes(node.tagName.toLowerCase()))
-                return resultNode;
+                return result;
 
             keepWhitespace ||= ["preserve", "preserve-spaces"].includes(nodeStyle.getPropertyValue("white-space-collapse"));
             keepWhitespace ||= node.tagName.toLowerCase() === "pre";
 
             if (nodeStyle.getPropertyValue("display") === "none")
                 resultNode.setAttribute("style", "display: none");
-            if (["hidden", "collapse"].includes(nodeStyle.getPropertyValue("visibility")))
+            else if (["hidden", "collapse"].includes(nodeStyle.getPropertyValue("visibility")))
                 resultNode.setAttribute("style", "visibility: hidden");
-            if (nodeStyle.getPropertyValue("opacity") === "0")
+            else if (nodeStyle.getPropertyValue("opacity") === "0")
                 resultNode.setAttribute("style", "opacity: 0");
+            else resultNode.setAttribute("style", "");
 
             const nChildren = node.childNodes.length;
             node.childNodes.forEach((child, idx) => {
                 const childResult = getSimplifiedDomRecursive(child, keepWhitespace);
-                if (childResult == null)
-                    return null;
-                if ((idx === 0 || idx === nChildren - 1) && nodeStyle.getPropertyValue("display") === "block" && childResult.nodeType === Node.TEXT_NODE && childResult.textContent === " ")
-                    return null;
-                if (idx > 0 && idx < nChildren - 1 && childResult.nodeType === Node.TEXT_NODE && childResult.textContent === " ")
-                    return null;
-                resultNode.appendChild(childResult);
+                if (!childResult.length)
+                    return;
+                for (const childNode of childResult) {
+                    if ((idx === 0 || idx === nChildren - 1) && nodeStyle.getPropertyValue("display") === "block" && childNode.nodeType === Node.TEXT_NODE && childNode.textContent === " ")
+                        continue;
+                    if (idx > 0 && idx < nChildren - 1 && childNode.nodeType === Node.TEXT_NODE && childNode.textContent === " ")
+                        continue;
+                    resultNode.appendChild(childNode);
+                }
             });
 
-            return resultNode;
+            return result;
         }
+        return [];
     }
 
     const simplifiedDomBody = getSimplifiedDomRecursive(rootElement, false);
-    if (simplifiedDomBody instanceof HTMLElement)
-        return simplifiedDomBody.outerHTML;
+    if (simplifiedDomBody)
+        return simplifiedDomBody.map(
+            element => (element instanceof HTMLElement) ? element.outerHTML : element instanceof Text ? element.textContent : ""
+        ).join("");
     return "";
 }
 
@@ -115,8 +136,7 @@ function enqueuePageAction(actionQueue: ActionQueue, action: ActionRequest) {
 
     if (llmPageActions.hasOwnProperty(actionName)) {
         actionQueue.enqueue(llmPageActions[actionName].atomicActions(element, actionParams));
-    }
-    else {
+    } else {
         console.log(`Action ${actionName} not found`)
     }
 }
--- src/helpers/geminiInterfacing.ts
+++ src/helpers/geminiInterfacing.ts
@@ -1,25 +1,28 @@
 import {GenerateContentRequest, GoogleGenerativeAI} from "@google/generative-ai";
 import {JSONParser} from "@streamparser/json";
 import {storageKeys, cssPrefix} from "./constants";
-import type {ParsedElementInfo} from "@streamparser/json/src/utils/types/parsedElementInfo";
+import type {ParsedElementInfo} from "@streamparser/json/dist/mjs/utils/types/ParsedElementInfo";
 import {getOutputFormatDescription} from "./promptParts";
 
 async function getJsonGeminiModel() {
     const outputDescription = getOutputFormatDescription();
+    const systemInstruction = `You are an AI assistant in the form of a Google Chrome extension. You fulfill user requests provided in form of text or voide audio recording. With the user's request you are usually given some details about the webpage the user is currently on (both screenshot of the webpage, as well as a simplified HTML representation of the webpage with ${cssPrefix}-prefixed CSS classes for HTML elements identification). You can use this context to provide the user with the information they need. You can also ask the user for more information if you need it. Your response should always be a ${outputDescription}`;
+    console.log("Gemini system instruction: ", systemInstruction);
+
     const GOOGLE_API_KEY = (await chrome.storage.sync.get([storageKeys.googleApiKey]))[storageKeys.googleApiKey];
     const gemini = (new GoogleGenerativeAI(GOOGLE_API_KEY)).getGenerativeModel(
         {
             model: "gemini-1.5-flash",
-            systemInstruction: `You are an AI assistant in the form of a Google Chrome extension. You fulfill user requests provided in form of text or voide audio recording. With the user's request you are usually given some details about the webpage the user is currently on (both screenshot of the webpage, as well as a simplified HTML representation of the webpage with ${cssPrefix}-prefixed CSS classes for HTML elements identification). You can use this context to provide the user with the information they need. You can also ask the user for more information if you need it. Your response should always be a ${outputDescription}`
+            systemInstruction: systemInstruction
         }
     );
     gemini.generationConfig.responseMimeType = "application/json";
     return gemini;
 }
 
-async function asyncRequestAndParse(requestData: GenerateContentRequest, jsonPaths: Array<string>, onValueCallback: (info: ParsedElementInfo) => void) {
+async function asyncRequestAndParse(requestData: GenerateContentRequest, jsonPaths: Array<string>, onValueCallback: (parsedElementInfo: ParsedElementInfo) => void) {
     const parser = new JSONParser({stringBufferSize: undefined, paths: jsonPaths});
-    // @ts-ignore
+
     parser.onValue = onValueCallback;
     parser.onError = (error) => console.log("Error while parsing JSON: ", error);
 
